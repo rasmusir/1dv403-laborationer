f1a88409ac17a5481347559933b4dc6f27716498
Rasmus Israelsson <israelssonrasmusraz@gmail.com>
2014-12-09 18:18:11 +0000

    Adds basic windows

--- 5-pwd/simplewindows/simplewindows.js
+++ 5-pwd/simplewindows/simplewindows.js
@@ -3,6 +3,10 @@ var sw;
 window.addEventListener("load",function()
 {
     sw = new SimpleWindows();
+    var w = new Window(sw);
+    
+    w.setPosition(100,100);
+    w.show();
 });
 
 function SimpleWindows()
@@ -10,5 +14,56 @@ function SimpleWindows()
     this.desktopElement = document.createElement("div");
     this.desktopElement.classList.add("desktop");
     
+    this.blurCanvas = document.createElement("canvas");
+    this.blur = this.blurCanvas.getContext("2d");
+    this.background = new Image();
+    this.background.src = "simplewindows/swirl_pattern.png";
+    
+    this.appbar = document.createElement("div");
+    this.appbar.classList.add("appbar");
+    
+    this.desktopElement.appendChild(this.appbar);
     document.body.appendChild(this.desktopElement);
 }
+
+SimpleWindows.prototype.updateBackground = function()
+{
+    this.blur.width = 50;
+    this.blur.height = 50;
+    this.blurCanvas.width = 50;
+    this.blurCanvas.height = 50;
+    this.blur.drawImage(this.background,0,0,50,50);
+    
+    this.appbar.style.backgroundImage = "url(" + this.blurCanvas.toDataURL() + ")";
+    
+};
+
+
+
+function Window(handler)
+{
+    this.handler = handler;
+    this.element = document.createElement("div");
+    this.element.classList.add("window");
+    this.top = document.createElement("div");
+    this.top.classList.add("top");
+    this.close = document.createElement("div");
+    this.close.classList.add("close");
+    this.content = document.createElement("div");
+    this.content.classList.add("content");
+    
+    this.top.appendChild(this.close);
+    this.element.appendChild(this.top);
+    this.element.appendChild(this.content);
+}
+
+Window.prototype.show = function()
+{
+    this.handler.desktopElement.appendChild(this.element);
+};
+
+Window.prototype.setPosition = function(x,y)
+{
+    this.element.style.left = x+"px";
+    this.element.style.top = y+"px";
+};
